13c23961148e7c259bd07ec92780715e23febf52
2022phyro <phyrokelstein2@gmail.com>
2022-11-27 21:39:51 +0100

    Create the alien cllass and add the necessary methods for moving the alien

--- Drawing.py
+++ Drawing.py
@@ -1,4 +1,8 @@
-from all_imports import *
+import pygame
+from pygame.sprite import Sprite
+from random import randint
+
+
 
 class Screen_settings:
     def __init__(self):
@@ -11,9 +15,7 @@ class Screen_settings:
 class Background:
     def __init__(self, screen):
         self.screen = screen
-        
-        self.image = pygame.image.load('The_background.bmp')
-        self.image.set_colorkey((0, 0, 0))
+        self.image = pygame.image.load('Images/The_background.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -31,7 +33,7 @@ class Spaceship:
         self.down = False
         self.game_settings = game_settings
         self.screen = screen
-        self.image = pygame.image.load('The_starship.bmp')
+        self.image = pygame.image.load('Images/The_starship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -44,7 +46,27 @@ class Spaceship:
     def draw_ship(self):
     #Draw the starship
         self.screen.blit(self.image, self.rect)
-        
+
+    def move_ship(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.right = True
+        if event.key == pygame.K_LEFT:
+            self.left = True
+        if event.key == pygame.K_UP:
+            self.up = True
+        if event.key == pygame.K_DOWN:
+           self.down = True
+
+    def stop_ship(self, event):
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.right = False
+            if event.key == pygame.K_LEFT:
+                self.left = False
+            if event.key == pygame.K_UP:
+                self.up = False
+            if event.key == pygame.K_DOWN:
+                self.down = False
     def update_ship_movements(self):
         if self.right and self.rect.right < self.screen_rect.right:
             self.center += self.game_settings.factor
@@ -57,4 +79,58 @@ class Spaceship:
             self.bottom += self.game_settings.factor
         self.rect.bottom = self.bottom
 
+class Alien(Sprite):
+    def __init__(self, screen, game_settings):
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.game_settings = game_settings
+        self.image = pygame.image.load('Images/The_Alien.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.centerx -= 50
 
+        self.rect.x  = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
+        self.x = float(self.rect.y)
+        self.fleet_speed = self.game_settings.factor
+        self.fleet_direction = 1
+    def draw_alien(self):
+        self.screen.blit(self.image, self.rect)
+    def update(self):
+        self.x += self.game_settings.factor - 2
+        self.rect.x = self.x
+    def rid_excess_fleet(self, aliens):
+        for bullet in aliens.copy():
+            if bullet.rect.bottom <= 0:
+               aliens.remove(bullet)
+    def update_the_fleet(self, aliens):
+        self.check_fleet_edges(aliens)
+        aliens.update()
+        self.rid_excess_fleet(aliens)
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return False
+        self.x += (self.game_settings.factor * self.fleet_direction)
+    def check_fleet_edges(self, aliens):
+        for alien in aliens.sprites():
+            if alien.check_edges():
+                alien.change_fleet(aliens)
+                break
+
+    def change_fleet(self, aliens):
+        for alien in aliens.sprites():
+            alien.rect.y += alien.game_settings.factor - 1.5
+            alien.fleet_direction *= -1
+
+    def create_fleet(self, aliens):
+        no_of_aliens = int(5)
+        for alien_serial in range(no_of_aliens):
+            new_alien = Alien(self.screen, self.game_settings)
+            new_alien.x = self.rect.width + randint(2,2) * self.rect.width * alien_serial
+            new_alien.rect.x = new_alien.x
+            aliens.add(new_alien)
---
+++ bullets.py
@@ -0,0 +1,86 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet_settings:
+     def __init__(self):
+        self.width = 1.5
+        self.height = 15
+        self.speed_factor = 3.5
+        self.color = (250, 0, 0)
+        self.doublegun = 25
+class Bullet(Sprite):
+    #A class to manage the bullets
+    def __init__(self, Bullet_settings, screen, ship):
+        super(Bullet, self).__init__()
+        self.screen = screen
+        self.ship = ship
+        self.bulset = Bullet_settings
+        self.rect = pygame.Rect(0, 0, self.bulset.width,
+                self.bulset.height)
+        self.rect.centerx = ship.rect.centerx
+        self.center = self.rect.centerx
+        self.rect.top = ship.rect.top
+        self.y = float(self.rect.y)
+        self.color = self.bulset.color
+        self.rect2 =  pygame.Rect(0, 0, self.bulset.width,
+                self.bulset.height)
+        self.rect2.centerx = ship.rect.centerx
+        self.rect2.top = ship.rect.top
+       
+        self.speed_factor = self.bulset.speed_factor
+        self.doublegun = self.bulset.doublegun
+
+    def update(self):
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+        self.rect2.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+    def draw_bullets(self, bullets):
+        for bullet in bullets.sprites():
+            # bullet.doubler()
+            # bullet.machinegun()
+            bullet.draw_bullet()
+
+    def doubler(self):
+        self.rect.centerx = self.center + self.doublegun
+        self.rect2.centerx = self.center - self.doublegun
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color, self.rect2)
+
+
+    def machinegun(self):
+        self.speed_factor += 2;
+
+    def move_bullets(self, event, bullets):
+        if event.key == pygame.K_SPACE and len(bullets) < 3:
+            new_bullet = Bullet(self.bulset, self.screen, self.ship)
+            bullets.add(new_bullet)
+
+    def rid_excess_bullets(self, bullets):
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
+    def update_the_catridge(self, bullets):
+        bullets.update()
+        self.rid_excess_bullets(bullets)
+
+
+class Alien_bullet(Bullet):
+    def __init__(self, Bullet_settings, screen, ship):
+        super(Alien_bullet, self).__init__(Bullet_settings, screen, ship)
+        self.rect.top = False
+        self.rect.bottom = ship.rect.bottom
+        self.rect2.bottom = ship.rect.bottom
+
+    def update(self):
+        self.y += self.speed_factor
+        self.rect.y = self.y
+        self.rect2.y = self.y
+
+    def move_bullets(self, bullets):
+        new_bullet = Alien_bullet(self.bulset, self.screen, self.ship)
+        bullets.add(new_bullet)
+
